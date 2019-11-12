7fd7fb96dc2853af4c676d9d1b33a32f903936b3
xuan wang <senoh510@gmail.com>
2019-11-13 00:38:51 +0900

    05  超市进销存管理系统

---
+++ SuperMarket/ADOConn.cpp
@@ -0,0 +1,86 @@
+// ADOConn.cpp: implementation of the ADOConn class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "SuperMarket.h"
+#include "ADOConn.h"
+
+#ifdef _DEBUG
+#undef THIS_FILE
+static char THIS_FILE[]=__FILE__;
+#define new DEBUG_NEW
+#endif
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+ADOConn::ADOConn()
+{
+
+}
+
+ADOConn::~ADOConn()
+{
+
+}
+
+void ADOConn::OnInitADOConn()
+{
+	try
+	{
+		m_pConnection.CreateInstance(_uuidof(Connection));
+		m_pConnection.CreateInstance(_uuidof(Recordset));
+       m_pConnection.CreateInstance("ADODB.Connection");
+	 //  _bstr_t strConnect="Provider=MSDAORA.1;Password=mr;User ID=mr;Data Source=orcl;Persist Security Info=True";
+	  _bstr_t strConnect="Provider=MSDAORA.1;Password=Mingri;User ID=system;Data Source=orcl;Persist Security Info=True";
+	   m_pConnection->Open(strConnect,"","",adModeUnknown);
+	}
+	catch(_com_error e)
+	{
+	//	AfxMessageBox(e.Description());
+		CString errormessage;
+		errormessage.Format("数据库连接失败！\r\n 错误信息:%s",e.ErrorMessage());
+		AfxMessageBox(errormessage);
+	}
+}
+
+BOOL ADOConn::ExecuteSQL(_bstr_t bstrSQL)
+{
+	try
+	{
+        if(m_pConnection==NULL)
+	     	OnInitADOConn();
+        m_pConnection->Execute(bstrSQL,NULL,adCmdText);
+		return true;
+	}
+	catch(_com_error e)
+	{
+		e.Description();
+		return false;
+	}
+}
+
+void ADOConn::ExitConnect()
+{
+    if(m_pRecordset != NULL)
+		m_pRecordset->Close();
+	m_pConnection->Close();
+}
+
+_RecordsetPtr& ADOConn::GetRecordSet(_bstr_t bstrSQL)
+{
+	try
+	{
+		if(m_pConnection==NULL)
+              OnInitADOConn();
+		m_pRecordset.CreateInstance(__uuidof(Recordset));
+        m_pRecordset->Open(bstrSQL,m_pConnection.GetInterfacePtr(),adOpenDynamic,adLockOptimistic,adCmdText);
+	}
+    catch(_com_error e)
+	{
+		e.Description();
+	}
+	return m_pRecordset;
+}
